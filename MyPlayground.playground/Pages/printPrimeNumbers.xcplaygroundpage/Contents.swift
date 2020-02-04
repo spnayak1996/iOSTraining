3900f1c82ecab5f4b42d81c5b6b94222826e78f9
Sarthak Pattanayak <sarthakpattanayak2@gmail.com>
2020-02-04 09:42:37 +0530

    Added comments to printPrimes

--- MyPlayground.playground/Pages/printPrimeNumbers.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/printPrimeNumbers.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Foundation
 
-func checkIfPrime(num: Int, primesList: [Int]) -> Bool {
+func checkIfPrime(num: Int, primesList: [Int]) -> Bool {//func to check if the given number is prime
     for prime in primesList {
         if prime > Int(pow(Double(num),0.5)) {
             break
@@ -14,7 +14,7 @@ func checkIfPrime(num: Int, primesList: [Int]) -> Bool {
     return true
 }
 
-func createPrimeList(n: Int) -> [Int] {
+func createPrimeList(n: Int) -> [Int] {//creates list of first n primes
     var primes = [2]
     
     var current = 3
@@ -28,7 +28,7 @@ func createPrimeList(n: Int) -> [Int] {
     return primes
 }
 
-func printPrimes(count n: Int) {
+func printPrimes(count n: Int) { //the function to be called to print primes
     guard n > 0  else {
         print("Invalid input")
         return
