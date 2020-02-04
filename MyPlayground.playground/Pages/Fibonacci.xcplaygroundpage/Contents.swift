9cc19d615730cf64b41754cb1a322396a49fc7bc
Sarthak Pattanayak <sarthakpattanayak2@gmail.com>
2020-02-04 12:27:34 +0530

    fibonacci

---
+++ MyPlayground.playground/Pages/Fibonacci.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+func nthFibonacci(n count: Int64) -> Int64 {
+    var fN_1: Int64 = 1
+    var fN: Int64 = 2
+    
+    if count <= 2 {
+        return 1
+    } else {
+        for i in 1...(count - 2) {
+            let temp = fN
+            fN = fN_1 + fN
+            fN_1 = temp
+        }
+        return fN
+    }
+}
+
+print(nthFibonacci(n: 91)) // can only calculate till n= 91
+
+
