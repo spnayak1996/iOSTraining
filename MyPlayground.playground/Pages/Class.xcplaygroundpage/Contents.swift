875cc9d425b64b302e4fee7ffb6a95450c37bb6b
Sarthak Pattanayak <sarthakpattanayak2@gmail.com>
2020-02-05 17:40:37 +0530

    class

---
+++ MyPlayground.playground/Pages/Class.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+
+class Circle {
+    var radius: Double              //stored property editable (variable)
+    let pi = 3.141592               //stored property non-editable (constant)
+    var area: Double {              //computed property with both get and set
+        get {
+            return pi * radius * radius
+        }
+        set(newArea) {
+            radius = (newArea/pi).squareRoot()
+        }
+    }
+    var perimeter: Double {         //computed property with only get
+        return 2 * pi * radius
+    }
+    
+    init(radius: Double) {          //initializer(designated)
+        self.radius = radius
+    }
+    
+    convenience init() {            //initializer(convenience)
+        self.init(radius: 1)
+    }
+    
+    func printDescription() {       //function that prints radius, perimeter and area
+        print("A circle with radius = \(radius), perimeter = \(self.perimeter), area = \(self.area)")
+    }
+}
+
+let circle1 = Circle()               //convenience init called
+let circle2 = Circle(radius: 2.5)    //designated init called
+
+circle1.printDescription()           //function call
+circle2.printDescription()
+
+circle1.area = 15
+
+print(circle1.radius, circle2.radius)//call to variable
+
+
