67a8181475932a60c8ff9b0592e843e1e4e53b55
Sarthak Pattanayak <sarthakpattanayak2@gmail.com>
2020-02-06 18:08:31 +0530

    added bill calculator

---
+++ MyPlayground.playground/Pages/BillCalculator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,150 @@
+import Foundation
+
+extension Double {
+    func roundTo2Decimal() -> Double {
+        return (self * 100).rounded()/100
+    }
+}
+
+struct Utils {
+    static func updateDict<T>(_ dict: inout [T: Int], _ key: T, _ change: Int) -> Bool {
+        if change > 0 {
+            dict[key] = (dict[key] ?? 0) + change
+            return true
+        } else if change < 0 {
+            if let qty = dict[key], qty > 0 {
+                if qty > (-change) {
+                    dict[key] = (dict[key] ?? 0) + change
+                }
+                dict[key] = nil
+                return true
+            }
+            return false
+        }
+        return true
+    }
+}
+
+class Category {
+    private(set) var name: String
+    private(set) var exempted: Bool = false
+    
+    init(name: String) {
+        self.name = name
+        if name == "Book" || name == "Food" || name == "Medicine" {
+            self.exempted = true
+        }
+    }
+}
+
+class Product {
+    private(set) var name: String                   //name is unique
+    private(set) var imported: Bool
+    private(set) var price: Double
+    private var category: Category
+    
+    init(name: String, imported: Bool, price: Double, category: Category) {
+        self.name = name
+        self.imported = imported
+        self.price = price.roundTo2Decimal()
+        self.category = category
+    }
+    
+    convenience init(name: String, imported: Bool, price: Double, categoryName: String) {
+        self.init(name: name, imported: imported, price: price, category: Category(name: categoryName))
+    }
+    
+    func calculateTax() -> Double {
+        var tax: Double = 0
+        if !self.category.exempted {
+            tax += price * 0.1
+        }
+        if self.imported {
+            tax += price * 0.05
+        }
+        return tax
+    }
+}
+
+extension Product: Hashable {
+    static func == (lhs: Product, rhs: Product) -> Bool {
+        if lhs.name == rhs.name{
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(self.name)
+    }
+}
+
+class Cart {
+    private var shoppingCart: [Product: Int]
+    
+    static let cart = Cart()
+    
+    private init() {
+        self.shoppingCart = [Product: Int]()
+    }
+    
+    func add(_ product: Product) {
+        if Utils.updateDict(&(Cart.cart.shoppingCart), product, 1) {
+            print("\(product.name) successfully added to cart.")
+        } else {
+            print("\(product.name) could not be added to cart.")
+        }
+    }
+    
+    func remove(_ product: Product) {
+        if Utils.updateDict(&(Cart.cart.shoppingCart), product, -1) {
+            print("\(product.name) successfully removed from cart.")
+        } else {
+            print("\(product.name), No such product in cart.")
+        }
+    }
+    
+    func printBill() {
+        print()
+        print("name      price      qty      tax")
+        var total: Double = 0
+        for key in Cart.cart.shoppingCart.keys.sorted(by: { $0.name < $1.name }) {
+            if let qty = Cart.cart.shoppingCart[key] {
+                let tax = key.calculateTax()
+                total += (key.price + tax) * Double(qty)
+                print("\(key.name)      \(key.price)      \(qty)      \(tax)")
+            }
+        }
+        print("Total = \(total.rounded())")
+        print()
+    }
+}
+
+//test
+let stationery = Category(name: "Stationery")
+
+
+let product1 = Product(name: "Resnik", imported: false, price: 5000, categoryName: "Book")
+let product2 = Product(name: "Pen", imported: true, price: 500, category: stationery)
+let product3 = Product(name: "Candy", imported: true, price: 100.456, categoryName: "Food")
+let product4 = Product(name: "Cetzine", imported: false, price: 150.557, categoryName: "Medicine")
+
+
+let cart = Cart.cart
+cart.add(product1)
+cart.remove(product4)
+cart.printBill()
+cart.add(product2)
+cart.add(product3)
+cart.add(product4)
+cart.add(product2)
+cart.add(product4)
+cart.add(product1)
+cart.remove(product1)
+cart.printBill()
+cart.add(product1)
+cart.add(product2)
+cart.add(product4)
+cart.add(product1)
+cart.printBill()
