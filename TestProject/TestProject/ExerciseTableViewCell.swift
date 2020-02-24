2831b6be80eb8d38a76e2ee5099726918d112fb2
Sarthak Pattanayak <sarthakpattanayak2@gmail.com>
2020-02-24 19:00:29 +0530

    changes

--- TestProject/TestProject/ExerciseTableViewCell.swift
+++ TestProject/TestProject/ExerciseTableViewCell.swift
@@ -23,15 +23,6 @@ class ExerciseTableViewCell: UITableViewCell {
         // Initialization code
     }
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.selectionStyle = .none
-    }
-    
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-    }
-    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
